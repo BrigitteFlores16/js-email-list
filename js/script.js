626bebd12039dac8d8449033cbef0ee3474366bd
BrigitteFlores16 <macalupufloresyiyibrigitte@gmail.com>
2024-11-06 17:59:14 +0100

    completato esercizio

--- js/script.js
+++ js/script.js
@@ -1,3 +1,28 @@
-function generatorEmails () {
 
+function generatorEmails() {
+    let emails = [];
+    for (let i = 0; i < 10; i++) {
+        fetch('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(response => response.json())
+            .then(data => {
+                emails.push(data.response);
+                if (emails.length === 10) {
+                    listEmail(emails);
+                }
+            })
+            
+    }
 }
+function listEmail(emails) {
+    let emailContainer = document.getElementById('email-list');
+    emailContainer.innerHTML = '';
+    emails.forEach(email => {
+        let listItem = document.createElement('li');
+        listItem.textContent = email;
+        listItem.classList.add('list-group-item');
+        emailContainer.appendChild(listItem);
+    });
+}
+
+
+document.getElementById('Emails-Button').addEventListener('click', generatorEmails);
